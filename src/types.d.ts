4cba5a6e15a65842e91177c7eaa1ae89c3a28300
Alexis Reyna Sánchez <99472437+Mapaches01@users.noreply.github.com>
2024-04-03 23:28:12 -0600

    Added file to work with .png and .mp3 in TypeScript

---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+// Code to work with png and mp3 files 
+
+declare module '*.png' {
+    const value: any;
+    export default value;
+}
+
+declare module '*.mp3' {
+    const value: any;
+    export default value;
+}
